207db676fb454510956ede50aa7f1ad83dff6b9b
Stanislavcod <stas.dryk@mail.ru>
2023-11-11 13:08:52 +0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+import BasketPage from "./BasketPage/BasketPage";
 import MenuPage from "./MenuPage/MenuPage";
 import MainPage from "./MainPage/MainPage";
+import LoginPage from './LoginPage/LoginPage';
 import SignInPage from "./SignInPage/SignInPage";
 import SignUpPage from "./SignUpPage/SignUpPage";
 
