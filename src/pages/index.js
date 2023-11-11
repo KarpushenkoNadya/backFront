5203f8810d11de29d139275648eae63c617822cf
Stanislavcod <stas.dryk@mail.ru>
2023-11-11 12:48:13 +0300

    error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import BasketPage from "./BasketPage/BasketPage";
 import MenuPage from "./MenuPage/MenuPage";
 import MainPage from "./MainPage/MainPage";
-import LoginPage from './LoginPage/LoginPage';
 import SignInPage from "./SignInPage/SignInPage";
 import SignUpPage from "./SignUpPage/SignUpPage";
 
